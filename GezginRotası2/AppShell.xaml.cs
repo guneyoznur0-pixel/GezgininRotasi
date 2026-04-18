809bf24c735f17a7c6cfbb0efe1af038e2664d2d
guneyoznur0-pixel <guneyoznur0@gmail.com>
2026-04-18 23:07:07 +0300

    ilk kurulum
    ilk kurulum yapıldı

---
+++ GezginRotası2/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GezginRotası2
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GezginRotası2/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GezginRotası2;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
